7bc2c7f22407abc2bcdb32bb69eb6ba7fa4f3777
imwaitingnow <alan.marshall987@gmail.com>
2023-11-08 16:40:56 +0000

    updated update url

--- scripts/bypass city (addon)/Automated Link Handling and Bypass.user.js
+++ scripts/bypass city (addon)/Automated Link Handling and Bypass.user.js
@@ -7,8 +7,8 @@
 // @match        https://bypass.city/bypass*
 // @match        https://justpaste.it/*
 // @grant        none
-// @downloadURL  https://github.com/imwaitingnow/skyline-scripts/raw/main/scripts/Automated%20Link%20Handling%20and%20Bypass.user.js
-// @updateURL    https://github.com/imwaitingnow/skyline-scripts/raw/main/scripts/Automated%20Link%20Handling%20and%20Bypass.user.js
+// @downloadURL  https://github.com/imwaitingnow/skyline-scripts/raw/main/scripts/bypass%20city%20(addon)/Automated%20Link%20Handling%20and%20Bypass.user.js
+// @updateURL    https://github.com/imwaitingnow/skyline-scripts/raw/main/scripts/bypass%20city%20(addon)/Automated%20Link%20Handling%20and%20Bypass.user.js
 // @licence      GPL-3.0-or-later
 // ==/UserScript==
 
